f88593e50f1f1658f3c641d4bf66e6447d1e7582
Bohdan <bromanenko333@gmail.com>
2025-12-11 19:28:07 +0200

    minor fixes with blog page

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         catalog: resolve(__dirname, "catalog/index.html"),
+        blog: resolve(__dirname, "blog/index.html"),
       },
     },
   },
